990800bcf312390c1bb050fa1c803c15e41f9e9d
somalypolandem <105309766+somalypolandem@users.noreply.github.com>
2022-05-10 19:16:43 +0200

    Add files via upload

---
+++ project 5.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+
+namespace Journal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Captain Houston's diary");
+            while (true)
+            {
+                Console.WriteLine("Write 'start' to begin writting & 'stop' to sign out.");
+                string read = Console.ReadLine();
+                if (read == "start")
+                {
+                    Diary();
+                }
+                else
+                if (read == "stop")
+                {
+                    Environment.Exit(0);
+                }
+
+            }
+
+
+            static void Diary()
+            {
+                Console.Clear();
+                Console.Write("Dear diary... \t");
+                string file = "Captainsdiary.txt";
+
+                using (StreamWriter Diary = File.Exists(file) ? File.AppendText(file) : new StreamWriter("captainsdiary.txt"))
+                {
+                    Diary.Flush();
+                    Diary.WriteLine($"Captain Houston's Log\n Date: {DateTime.Now} ");
+
+
+                    while (true)
+                    {
+                        string input = Console.ReadLine();
+
+                        if (input == "stop")
+                        {
+                            Diary.Flush();
+
+                            Environment.Exit(0);
+                        }
+
+                        Diary.WriteLine(input);
+                        Diary.WriteLine("Cptn H.~");
+
+
+                    }
+
+                }
+
+            }
+        }
+    }
+}
